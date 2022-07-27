96c3a2cb1a71fc9966fe19796e42837bef846dd4
Jordan Farelli <jordanfarelli@gmail.com>
2022-07-26 21:06:12 -0600

    Created functionality to determine ingredients needed

--- test/Pantry-test.js
+++ test/Pantry-test.js
@@ -1,26 +1,26 @@
 import { expect } from 'chai'; 
-import Ingredient from '../src/classes/Ingredient';
 import Pantry from '../src/classes/Pantry';
 import Recipe from '../src/classes/Recipe';
 import User from '../src/classes/User';
+
 import usersData from '../src/sample-data/user-sample-data';
 import recipeData from '../src/sample-data/recipe-sample-data';
 import ingredientsData from '../src/sample-data/ingredient-sample-data';
 
-
 describe('Pantry', ( ) => {
 
     let user1;
-    let recipe1;
     let pantry1;
-    let ingredients1
+    let recipe1;
 
     beforeEach( ( ) => {
+        
         user1 = new User( usersData[0] )
-        ingredients1 = new Ingredient( ingredientsData )
-        recipe1 = new Recipe( recipeData[0] )
-        pantry1 = new Pantry( user1, ingredients1, recipe1 );
-            
+
+        recipe1 = new Recipe( recipeData[0], ingredientsData )
+        console.log('RECIPE1: ', recipe1)
+        pantry1 = new Pantry( user1 );
+        
         } );
         
     it( 'should be a function', ( ) => {
@@ -34,13 +34,14 @@ describe('Pantry', ( ) => {
     it( 'should take in a user\'s PANTRY ingredients', ( ) => {
         expect( pantry1.currentUsersPantry ).to.deep.equal( usersData[0].pantry )
     } );
-    
-    it( 'should take in a list of INGREDIENTS', ( ) => {
-        expect( pantry1.ingredientsList.id ).to.equal( ingredientsData )
-    } );
-    
-    it( 'should take in a list of RECIPE ingredients', ( ) => {
-        console.log('RECIPEDATA[0]: ', recipeData[0].ingredients)
-        expect( pantry1.recipeIngredients ).to.equal( recipeData[0].ingredients )
+
+    it( 'should get missing ingredients needed for recipe', ( ) => {
+        expect( pantry1.getNeededIngredients( recipe1 ) ).to.deep.equal
+        ( 
+            [ 
+                { id: 19334, quantity: { amount: 0.5, unit: 'c' } },
+                { id: 1012047, quantity: { amount: 24, unit: 'servings' } }
+            ] 
+        )
     } );
 } );
