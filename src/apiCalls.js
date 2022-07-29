9863e733ddf330474e41a044cb46d944287d31fb
Jordan Farelli <jordanfarelli@gmail.com>
2022-07-29 11:36:25 -0600

    Created functionality to POST data

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,18 @@
 export let getData = ( dataType ) => {
-    return fetch( `https://what-s-cookin-starter-kit.herokuapp.com/api/v1/${ dataType }` )
+    return fetch( `http://localhost:3001/api/v1/${ dataType }` )
         .then( response => response.json() )
-        .catch( error => console.log( error ) );
+        .catch( error => console.log( error ) ); // Need to add error handling
+}
+
+
+export let postData = ( data ) => {
+    return fetch( 'http://localhost:3001/api/v1/users', {
+        method: 'POST',
+        body: JSON.stringify( data ),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then( response => response.json( ) )
+    .catch( error => console.log( error ) ); // Need to add error handling
 }
