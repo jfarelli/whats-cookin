cb77ee9d2fdc948720731c4ff49d9f13df14d6fb
Jordan Farelli <jordanfarelli@gmail.com>
2022-07-29 23:48:56 -0600

    WIP: Getting modal to display ingredients discrepencies

--- src/classes/Pantry.js
+++ src/classes/Pantry.js
@@ -3,23 +3,16 @@ import Recipe from '../classes/Recipe';
 class Pantry {
     constructor( pantryData, ingredientData ) {
         this.currentUsersPantry = pantryData.pantry;
-        this.ingredientsList = ingredientData;
-        // after running the naming function - this.ingredientsList is returning as undefined, but the pantry is assigning names.  
-        // i don't know if that impacts us further downstream, but help would be appreciated understanding this one.
-        // i could see it being a result of the map() but not entirely sure 
-        this.ingredientsNeeded = [ ];      
+        this.ingredientsList = ingredientData;  
     };
 
-   // Determine whether a user's pantry has enough ingredients to cook a given recipe.
-getNeededIngredients( recipe, ingredientData ) {
-    // console.log("pantry class recipe: ", recipe)
+getMissingIngredients( recipe, ingredientData ) {
+    let ingredientsNeeded = [ ];
     let newRecipe = new Recipe( recipe, ingredientData )
         recipe.ingredients.forEach( recipeIngredient => {
-            let foundIngredients = this.currentUsersPantry.find( pantryItem => {
-                // console.log(pantryItem)
-                pantryItem.ingredient === recipeIngredient.id })
+            let foundIngredients = this.currentUsersPantry.find( pantryItem => pantryItem.ingredient === recipeIngredient.id )
             if( !foundIngredients ) {
-                this.ingredientsNeeded.push
+                ingredientsNeeded.push
                 ( 
                     { 
                         name: recipeIngredient.name = newRecipe.getIngredientsWithNames( [recipeIngredient], ingredientData )[0],
@@ -28,36 +21,37 @@ getNeededIngredients( recipe, ingredientData ) {
                     }
                 )
             } 
+            
         } )
-        return this.ingredientsNeeded
+        return ingredientsNeeded
 }
 
-// Determine the AMOUNT of missing ingredients still needed to cook a given recipe, 
-// based on what's in the user's pantry.
 
 getIngredientAmountNeeded( recipe, ingredientData ) {
+    let ingredientAmountNeeded = [ ];
     let newRecipe = new Recipe( recipe, ingredientData )
     recipe.ingredients.forEach( recipeIngredient => {
-        let foundIngredients = this.currentUsersPantry.find( pantryItem => pantryItem.ingredient === recipeIngredient.id )
-        if( foundIngredients && recipeIngredient.quantity.amount >= foundIngredients.amount ) {
-            this.ingredientsNeeded.push
-                ( 
-                    { 
-                        name: recipeIngredient.name = newRecipe.getIngredientsWithNames( [recipeIngredient], ingredientData )[0],
-                        id: recipeIngredient.id,
-                        quantity: recipeIngredient.quantity.amount - foundIngredients.amount
-                    }
-                )
-        }
+        let foundIngredientAmountDifference = this.currentUsersPantry.find( pantryItem => pantryItem.ingredient === recipeIngredient.id && pantryItem.amount < recipeIngredient.quantity.amount )
+        if( foundIngredientAmountDifference ) {
+            ingredientAmountNeeded.push
+            ( 
+                { 
+                    name: recipeIngredient.name = newRecipe.getIngredientsWithNames( [recipeIngredient], ingredientData )[0],
+                    id: recipeIngredient.id,
+                    quantity: ( recipeIngredient.quantity.amount - foundIngredientAmountDifference.amount )
+                }
+            )
+        } 
     }) 
+    return ingredientAmountNeeded
 }
 
-getPantryItemsWithNames( ingredientList ) {
-    let namedPantryIngredient = this.currentUsersPantry.map( ingredient => { 
-        ingredient.name = ingredientList.find( ing => ing.id == ingredient.ingredient ).name;
-        return ingredient.name;
-        });
-        return namedPantryIngredient
+
+getPantryItemsWithNames( recipe, ingredientList ) {
+    return recipe.ingredients.map( recipeIngredient => {
+        recipeIngredient.name = ingredientList.filter( ingredient => ingredient.id === recipeIngredient.id )[0].name
+        return recipeIngredient
+        } )
     };      
 };
   
